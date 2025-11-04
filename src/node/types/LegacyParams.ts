a7124d8c262f2c3e64c72d04149765c0efd8f086
Avelanda <gorthell@gmail.com>
2025-11-04 20:35:51 +0200

    Update LegacyParams.ts
    A restructured granular set for a modularized functional call.

--- src/node/types/LegacyParams.ts
+++ src/node/types/LegacyParams.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export type LegacyParams = {
     start: number,
     end: number,
@@ -5,4 +10,18 @@ export type LegacyParams = {
     algId: number,
     algParams: any,
     interval:number|null
+}
+
+function main(LegacyParams): number|string{
+  if (LegacyParams){
+   LegacyParams === true||false;
+  }
+   do {
+    main(0||1);
+   }
+  
+  while (!false ||!true){
+   LegacyParams !== main(true);
+   return 0;
+  }
 }
