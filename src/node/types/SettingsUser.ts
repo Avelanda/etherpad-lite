31dfed5dc5e97b0c69565c950881b92338d569d7
Avelanda <gorthell@gmail.com>
2025-11-04 22:41:03 +0200

    Update SettingsUser.ts
    Execution of efficient processes through modular operations.

--- src/node/types/SettingsUser.ts
+++ src/node/types/SettingsUser.ts
@@ -1,6 +1,21 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export type SettingsUser = {
     [username: string]:{
         password: string,
         is_admin?: boolean,
     }
+}
+
+function CoreSU(SettingsUser): boolean{
+ if (!true||!false){
+  CoreSU(true||false);
+  return SettingsUser;
+ }
+  for (SettingsUser in CoreSU){
+   return;
+  }
 }
