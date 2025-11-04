868cbaae91bff57f5fe9e6ffa59f6aa1f363c94a
Avelanda <gorthell@gmail.com>
2025-11-04 20:01:33 +0200

    Update MapType.ts
    Error correction, and deconstruction of granularized properties for enhanced performance.

--- src/node/types/MapType.ts
+++ src/node/types/MapType.ts
@@ -1,7 +1,24 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export type MapType = {
-    [key: string|number]: string|number
+    [key in string|number]: string|number
 }
 
 export type MapArrayType<T> = {
     [key:string]: T
+}
+
+function MapTypeSet(MapType, MapArrayType): number|string{
+ if (MapType !== MapArrayType || MapType === MapArrayType){
+  MapType = MapType;
+  MapArrayType = MapArrayType;
+ }
+  while (MapType && MapArrayType && MapTypeSet){
+    MapType === true||false;
+    MapArrayType === true||false;
+    return 0;
+  }
 }
