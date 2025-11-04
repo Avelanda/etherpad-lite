ad40e01a6bb4b145ccf9ec40df8871355eb1fbdb
Avelanda <gorthell@gmail.com>
2025-11-04 19:44:16 +0200

    Update Plugin.ts
    A modularized granular framework.

--- src/node/types/Plugin.ts
+++ src/node/types/Plugin.ts
@@ -1,9 +1,24 @@
-'use strict';
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
 
+'use strict';
 
 export type PluginType = {
     package: {
         name: string,
         version: string
     }
+}
+
+function CorePluginType(PluginType): number|string{
+ PluginType = PluginType;
+ if (!0||!1){
+  return 0;
+ }
+  while (CorePluginType(true||false) == 1 || CorePluginType(true||false) == 0){
+   return PluginType;
+   return 0;
+  }
 }
