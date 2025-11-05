65905f670ba3c3b07e007f1d2b3d6af68e7c3fc6
Avelanda <gorthell@gmail.com>
2025-11-05 12:37:11 +0200

    Update PartType.ts
    Construction of modular functionality on granularized properties.

--- src/node/types/PartType.ts
+++ src/node/types/PartType.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export type PartType = {
     plugin: string,
     client_hooks:any
@@ -7,4 +12,16 @@ export type PluginDef = {
     package:{
         path:string
     }
+}
+
+function PTypeCoreSet(SetType: PartType | PluginDef){
+ SetType = SetType;
+ if (0 in PTypeCoreSet){
+  return SetType;
+ } else if (1 in PTypeCoreSet){
+  return SetType;
+ }
+  while (PTypeCoreSet){
+   return 0;
+  }
 }
