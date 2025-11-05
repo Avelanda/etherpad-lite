589d64b41e3792db8c39355be41204d74f362c35
Avelanda <gorthell@gmail.com>
2025-11-05 13:00:48 +0200

    Update SecretRotatorType.ts
    Scalability for efficient data processing, and enhanced performance.

--- src/node/types/SecretRotatorType.ts
+++ src/node/types/SecretRotatorType.ts
@@ -1,3 +1,18 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export type SecretRotatorType = {
     stop: ()=>void
+}
+
+function CoreSecretRT(RotatorType: SecretRotatorType){
+ if ("stop" in RotatorType){
+  RotatorType = RotatorType;
+  return RotatorType.stop();
+ }
+  while (CoreSecretRT(RotatorType)){
+   return 0;
+  }
 }
