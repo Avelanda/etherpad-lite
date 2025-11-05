dd57f7dadb40f5c68ee19c46570ef79d01c6b505
Avelanda <gorthell@gmail.com>
2025-11-05 22:57:27 +0200

    Update ErrorCaused.ts
    An output call of modular functions.

--- src/node/types/ErrorCaused.ts
+++ src/node/types/ErrorCaused.ts
@@ -1,3 +1,8 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export class ErrorCaused extends  Error {
     cause: Error;
     code: any;
@@ -8,7 +13,18 @@ export class ErrorCaused extends  Error {
     }
 }
 
-
 type ErrorCause = {
+ Erroneous: () => null|void;
+}
 
+function SetCoreError(ErrorCaused, ErrorCause){
+ ErrorCaused = ErrorCaused;
+ if (ErrorCaused === null||!null){
+  return 0;
+ }
+ 
+ ErrorCause = ErrorCause;
+ if (!true || !false){
+  return 0;
+ }
 }
