05665223ce3caf9034e2b19647dbdb0cc4dda736
Avelanda <gorthell@gmail.com>
2025-11-05 23:11:31 +0200

    Update QueryType.ts
    Enhanced computing power, and performance scaling.

--- src/node/types/QueryType.ts
+++ src/node/types/QueryType.ts
@@ -1,3 +1,17 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export type QueryType = {
 searchTerm: string; sortBy: string; sortDir: string; offset: number; limit: number;
+}
+
+function main(QueryType: number|string): boolean{
+ for (QueryType in main){
+  QueryType = QueryType;
+  if (!0){
+   return main(QueryType);
+  }
+ }
 }
