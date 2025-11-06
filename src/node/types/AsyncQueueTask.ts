a1c86d2fc2ab1cc6ac971d550cd03c53e0760f82
Avelanda <gorthell@gmail.com>
2025-11-06 13:16:39 +0200

    Update AsyncQueueTask.ts
    A modular set of a class function.

--- src/node/types/AsyncQueueTask.ts
+++ src/node/types/AsyncQueueTask.ts
@@ -1,5 +1,23 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export type AsyncQueueTask = {
     srcFile: string,
     destFile: string,
     type: string
+}
+
+function main(): string|number{
+ class AsyncQTSet <AsyncQueueTask> {
+  CoreAQTask: AsyncQueueTask;
+ };
+  if (AsyncQTSet){
+   return 0;
+  }
+   while (AsyncQueueTask => AsyncQTSet){
+    return main();
+   }
+    return 0;
 }
