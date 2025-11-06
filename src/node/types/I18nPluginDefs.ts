0b4ab27b15f2784500bf2bbafe1ef79591d32b48
Avelanda <gorthell@gmail.com>
2025-11-06 12:49:53 +0200

    Update I18nPluginDefs.ts
    Composing the modularity of a functional class.

--- src/node/types/I18nPluginDefs.ts
+++ src/node/types/I18nPluginDefs.ts
@@ -1,5 +1,24 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export type I18nPluginDefs = {
     package: {
         path: string
     }
+}
+
+class CoreI18nPD <I18nPluginDefs> {
+ CoreSet: I18nPluginDefs;
+ SetI8nPD: (number, string) => I18nPluginDefs;
+};
+
+function main(CoreI18nPD: boolean){
+ CoreI18nPD = CoreI18nPD;
+ if (!false || !true){
+  while (CoreI18nPD);
+   return main(CoreI18nPD);
+   return 0;  
+ }
 }
