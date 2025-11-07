56c7dc1125e011a76487b4f90f43204f23950cb5
Avelanda <gorthell@gmail.com>
2025-11-07 13:57:41 +0200

    Update DeriveModel.ts
    A class implementation on type function.

--- src/node/types/DeriveModel.ts
+++ src/node/types/DeriveModel.ts
@@ -1,6 +1,15 @@
+/*
+ * Copyright © 2025 Avelanda.
+ * All rights reserved.
+ */
+
 export type DeriveModel = {
     digest: string,
     secret: string,
     salt: string,
     keyLen: number
 }
+
+class DeriveModelSet <DeriveModel> {
+ CoreDMset: (number, string) => DeriveModel;
+};
